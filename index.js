8ed460594c4127456029a72aad9a6c3cbd9ac77e
Tariqul <tariqularmsfitt018@gmail.com>
2023-05-19 13:01:18 +0600

    Received toy from client side and post it to backend

--- index.js
+++ index.js
@@ -26,13 +26,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const toyCollection = client.db("toyTroveDB").collection("toy");
+    app.post("/", async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+      const result = await toyCollection.insertOne(newToy);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
