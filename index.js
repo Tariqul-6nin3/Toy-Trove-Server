f48672812349acdb78dc4009b1100b50de327b1a
Tariqul <tariqularmsfitt018@gmail.com>
2023-05-21 23:50:36 +0600

    vercel configuration file updated

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-// middleware
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+};
+// app.use(cors())
+app.use(cors(corsConfig));
+// PushSubscriptionOptions("", cors(corsConfig))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.TOY_USER}:${process.env.TOY_PASS}@cluster0.jsico6b.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     const toyCollection = client.db("toyTroveDB").collection("toy");
 
@@ -66,6 +72,16 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/searchedtoys", async (req, res) => {
+      let query = {};
+      if (req.query?.name) {
+        query = { name: req.query.name };
+      }
+      const result = await toyCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
     app.delete("/mytoys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -93,6 +109,26 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/mysortedtoys", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = toyCollection.find(query).sort({ price: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/mysortedtoyslowtohigh", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = toyCollection.find(query).sort({ price: 1 });
+      const result = await cursor.toArray();
       res.send(result);
     });
 
